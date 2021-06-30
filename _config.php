93afd8328c5514f4095412c2017c7c5b10ea1ab3
micschk <mic@restruct.nl>
2021-06-30 13:31:03 +0200

    Removing _config.php

--- _config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Restruct\SilverStripe\StreamVideo\CloudflareStreamShortcode;
-use SilverStripe\View\Parsers\ShortcodeParser;
-
-ShortcodeParser::get('default')->register('cloudflare_stream', [CloudflareStreamShortcode::class, 'cloudflare_stream']);
